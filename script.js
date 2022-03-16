7f63952971c6b02d12b8415076fec908150e917a
stantoha <78742661+stantoha@users.noreply.github.com>
2022-03-16 15:31:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,88 @@
-   let humans=document.querySelectorAll('.human');
-    let  i=-1;
-    
-    function humanHide(){
-        humans.forEach(item=>{
-            item.classList.add('hide');
-        });
-    }
+   let humans = document.querySelectorAll('.human');
+   let i = -1;
+   let j = Number;
 
-    humanHide();
+   function humanHide() {
+       humans.forEach(item => {
+           item.classList.add('hide');
+       });
+   }
 
-let showHuman=function(){
-    i++;
-    if(i>=0 && i<humans.length){
-        if(humans[i].classList.contains('hide')){
-            humans[i].classList.remove('hide'); 
-            humans[i].classList.add('fade'); 
+   humanHide();
+
+   let showHuman = function () {
+       i++;
+       if (i >= 0 && i < humans.length) {
+           if (humans[i].classList.contains('hide')) {
+               humans[i].classList.remove('hide');
+               humans[i].classList.add('fade');
+           }
+       } else {
+           if (i === humans.length) {
+               i = 0;
+               showHuman();
+           }
+       }
+
+
+       setTimeout(showHuman, 2000);
+
+   };
+   setTimeout(showHuman, 2000);
+
+
+   let rpgHuman=document.querySelector('.rpg__human'),
+   head=rpgHuman.querySelector('.head'),
+   neck=rpgHuman.querySelector('.neck'),
+   hand=rpgHuman.querySelector('.hand'),
+   leg=rpgHuman.querySelector('.leg'),
+   rightHandBottom=rpgHuman.querySelector('.right .hand__section__bottom'),
+   leftHandBottom=rpgHuman.querySelector('.left .hand__section__bottom'),
+   leftLegTop=rpgHuman.querySelector('.left .leg__section__top'),
+   rightLegTop=rpgHuman.querySelector('.right .leg__section__top'),
+   rightLegBottom=rpgHuman.querySelector('.right .leg__section__bottom'),
+   leftLegBottom=rpgHuman.querySelector('.left .leg__section__bottom'),
+   rightKnee=rpgHuman.querySelector('.right .knee'),
+   leftKnee=rpgHuman.querySelector('.left .knee'),
+   chest=rpgHuman.querySelector('.chest'),
+   hips=rpgHuman.querySelector('.hips'),
+   footLeft=rpgHuman.querySelector('.rpg__human .left .foot'),
+   footRight=rpgHuman.querySelector('.rpg__human .right .foot'),
+   rocket=rpgHuman.querySelector('.rocket'),
+   klasses=['rpg__right__hand__skew','left__leg__section__bottom','right__leg__section__bottom',
+   'right__knee','right__leg__section__top','rpg__foot','rpg__human__body'];
+
+   let  changePos=function(item,j){
+    if(showHuman){
+   item.classList.add(klasses[j]);
     }
-    }
-    else{
-        if(i===humans.length){
-            i=0;
-            showHuman();
-        }
-    }
-    
+   } 
    
-    setTimeout(showHuman,2000);
+   let changeBodyPos=function(){
+    changePos(rightHandBottom,0);
+    changePos(leftLegBottom,1);
+    changePos(rightLegBottom,2);
+    changePos(rightKnee,3);
+    changePos(rightLegTop,4);
+    changePos(footLeft,5);
+    changePos(footRight,5);
+    changePos(rpgHuman,6);
+   }
 
-};
-setTimeout(showHuman,2000);
+
+   let launch=setTimeout(changeBodyPos,6500);
+   
+
+   let rocketLaunch=function(){
+       launch;
+       rocket.classList.remove('rocket');
+       rocket.classList.add('rocket__launch');
+   }
+
+
+   setTimeout(rocketLaunch,8000) ;
+
+
+
+
 
